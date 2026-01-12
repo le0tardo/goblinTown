f0ec2696580802fe758a535ec5a9517282b21ca9
Leo Lutecki <leo.lutecki@lbs.se>
2026-01-12 07:22:23 +0100

    moved unit spawn

--- Assets/Village/Buildings/Domiciles/DomicileBehaviour.cs
+++ Assets/Village/Buildings/Domiciles/DomicileBehaviour.cs
@@ -1,14 +1,96 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DomicileBehaviour : MonoBehaviour
 {
     public int goblinCapIncrease = 1;
     public int level = 1;
 
+    public VillageResource resource;
+    [SerializeField] GameObject unit;
+    public float required;
+    public float available;
+    [SerializeField] public float spawnTime;
+    [SerializeField] public float maxSpawnTime;
+    public bool spawning = false;
+    public bool canSpawn=false;
+    Vector3 spawnPosition;
+
     private void Start()
     {
         UnitManager.inst.maxUnits += goblinCapIncrease;
+        spawnPosition = Vector3.zero;
     }
+    private void Update()
+    {
+        if (VillageResourceManager.inst.villageResources.TryGetValue(resource, out int a))
+        {
+            available = a;
+        }
+        else
+        {
+            available = 0;
+        }
+
+        if (UnitManager.inst.units.Count < UnitManager.inst.maxUnits && required <= available &&!spawning)
+        {
+            canSpawn = true;
+        }
+        else
+        {
+            canSpawn = false;
+        }
+
+        if (spawning)
+        {
+            if (spawnTime > 0)
+            {
+                spawnTime -= Time.deltaTime;
+            }
+            else
+            {
+                SpawnUnit();
+            }
+        }
+    }
+
+    public void StartSpawn() //call this from button
+    {
+        spawning = true;
+        Vector3 spawnNear = BuildingManager.inst.selectedBuilding.transform.position;
+        spawnPosition = GetNavMeshPointRadial(spawnNear, 2f, 3f);
+    }
+    Vector3 GetNavMeshPointRadial(Vector3 center, float minRadius, float maxRadius)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            // Random direction on XZ plane
+            Vector2 dir2D = Random.insideUnitCircle.normalized;
+            Vector3 dir = new Vector3(dir2D.x, 0f, dir2D.y);
+
+            // Pick distance away from building
+            float distance = Random.Range(minRadius, maxRadius);
+
+            Vector3 candidate = center + dir * distance;
+
+            if (NavMesh.SamplePosition(candidate, out NavMeshHit hit, 2f, NavMesh.AllAreas))
+                return hit.position;
+        }
+
+        Debug.LogWarning("No valid NavMesh point found near building");
+        return center;
+    }
+    public void SpawnUnit()
+    {
+        int cost = Mathf.RoundToInt(required);
+        VillageResourceManager.inst.RemoveResource(resource, cost);
+
+        Instantiate(unit, spawnPosition, Quaternion.identity);
+
+        spawnTime = maxSpawnTime;
+        spawning = false;
+    }
+
     private void OnDestroy()
     {
         UnitManager.inst.maxUnits -= goblinCapIncrease;
--- Assets/Village/Buildings/Domiciles/DomicileGUI.cs
+++ Assets/Village/Buildings/Domiciles/DomicileGUI.cs
@@ -6,15 +6,6 @@ using TMPro;
 
 public class DomicileGUI : MonoBehaviour
 {
-    public VillageResource resource;
-    [SerializeField] GameObject unit;
-    public float required;
-    public float available;
-    [SerializeField] float spawnTime;
-    [SerializeField] float maxSpawnTime;
-    public bool spawning = false;
-    Vector3 spawnPosition;
-
     DomicileBehaviour dom;
 
     [Header("UI")]
@@ -26,92 +17,38 @@ public class DomicileGUI : MonoBehaviour
 
     private void Start()
     {
-        unitCost.text = required.ToString();
-        costResource.sprite=resource.resourceIcon;
-        spawnPosition = Vector3.zero;
+        if (BuildingManager.inst.selectedBuilding == null) return;
+        dom = BuildingManager.inst.selectedBuilding.GetComponent<DomicileBehaviour>();
+        if (dom != null)
+        {
+            unitCost.text = dom.required.ToString();
+            costResource.sprite = dom.resource.resourceIcon;
+        }
     }
 
     private void Update()
     {
+        if(BuildingManager.inst.selectedBuilding==null)return;
         dom=BuildingManager.inst.selectedBuilding.GetComponent<DomicileBehaviour>();
-        if (dom == null){return;}
-
-        spawnProgression.fillAmount = spawnTime / maxSpawnTime;
-        unitCount.text=UnitManager.inst.units.Count.ToString()+"/"+UnitManager.inst.maxUnits.ToString();
-        SetColors();
-
-        if(VillageResourceManager.inst.villageResources.TryGetValue(resource,out int a))
+        if (dom != null)
         {
-            available=a;
+            spawnProgression.fillAmount = dom.spawnTime / dom.maxSpawnTime;
+            unitCount.text = UnitManager.inst.units.Count.ToString() + "/" + UnitManager.inst.maxUnits.ToString();
+            SetColors();
+            spawnButton.interactable = dom.canSpawn;
         }
-        else
-        {
-            available= 0;   
-        }
-
-        if (UnitManager.inst.units.Count < UnitManager.inst.maxUnits && required<=available)
-        {
-            spawnButton.interactable = true;
-        }
-        else
-        {
-            spawnButton.interactable= false;
-        }
-
-        if (spawning)
-        {
-            if (spawnTime > 0)
-            {
-                spawnTime -= Time.deltaTime;
-            }
-            else
-            {
-                SpawnUnit();
-            }
-        }
-
     }
-
-    public void StartSpawn() //call this from button
+   
+    public void ClickDomicile()
     {
-        spawning = true;
-        Vector3 spawnNear = BuildingManager.inst.selectedBuilding.transform.position;
-        spawnPosition = GetNavMeshPointRadial(spawnNear,2f,3f);
-    }
-    Vector3 GetNavMeshPointRadial(Vector3 center,float minRadius,float maxRadius)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            // Random direction on XZ plane
-            Vector2 dir2D = Random.insideUnitCircle.normalized;
-            Vector3 dir = new Vector3(dir2D.x, 0f, dir2D.y);
-
-            // Pick distance away from building
-            float distance = Random.Range(minRadius, maxRadius);
-
-            Vector3 candidate = center + dir * distance;
-
-            if (NavMesh.SamplePosition(candidate, out NavMeshHit hit, 2f, NavMesh.AllAreas))
-                return hit.position;
-        }
-
-        Debug.LogWarning("No valid NavMesh point found near building");
-        return center;
-    }
-    public void SpawnUnit()
-    {
-        int cost = Mathf.RoundToInt(required);
-        VillageResourceManager.inst.RemoveResource(resource, cost);
-
-        Instantiate(unit,spawnPosition,Quaternion.identity);
-        
-        spawnTime = maxSpawnTime;
-        spawning = false;
+        dom = BuildingManager.inst.selectedBuilding.GetComponent<DomicileBehaviour>();
+        if (dom == null) { return; }
+        dom.StartSpawn();
     }
 
     void SetColors()
     {
-        if (required > available)
+        if (dom.required > dom.available)
         {
             unitCost.color = Color.red;
         }
