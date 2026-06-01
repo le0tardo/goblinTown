7b2cadb09e0cc5bc5ce69b1897517ec0f40fc905
Leo Lutecki <leo.lutecki@learnet.se>
2026-06-01 15:43:53 +0200

    pickup respawns

--- Assets/Forageable/pickupObject/PickupBehaviour.cs
+++ Assets/Forageable/pickupObject/PickupBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PickupBehaviour : MonoBehaviour, IPickupable, IHoverable
@@ -13,17 +14,23 @@ public class PickupBehaviour : MonoBehaviour, IPickupable, IHoverable
 
     bool claimed=false;
 
+    [SerializeField] int waitSeconds;
+    SphereCollider coll;
+
+    [SerializeField] bool respaw;
+
     private void Start()
     {
         displayName =resource.resourceName;
         rend = GetComponentInChildren<MeshRenderer>();
         filt = GetComponentInChildren<MeshFilter>();
-
+        respaw = true;
         filt.mesh=resource.mesh;
         rend.material=resource.material;
 
         float randomY = Random.Range(0f, 360f);
         transform.rotation = Quaternion.Euler(0f, randomY, 0f);
+        coll=GetComponent<SphereCollider>();
     }
 
     public bool TryPickup(Unit unit)
@@ -36,7 +43,49 @@ public class PickupBehaviour : MonoBehaviour, IPickupable, IHoverable
         if(unit.carriedResource==null)unit.carriedResource = resource;
         unit.carriedAmount++;
 
-        Destroy(gameObject);
+        if (respaw)
+        {
+            StartCooldown();
+        }
+        else
+        {
+           Destroy(gameObject);
+        }
         return true;
     }
+
+    public void StartCooldown()
+    {
+        StartCoroutine(DisableTemporarily());
+    }
+
+    IEnumerator DisableTemporarily()
+    {
+        rend.enabled = false;
+        coll.enabled = false;
+
+        yield return new WaitForSeconds(waitSeconds);
+
+        CheckOverlappingColliders();
+
+        rend.enabled = true;
+        coll.enabled = true;
+        claimed=false;
+    }
+
+    void CheckOverlappingColliders()
+    {
+        float radius = coll.radius * transform.lossyScale.x;
+
+        Collider[] overlaps = Physics.OverlapSphere(
+            transform.position,
+            radius
+        );
+
+        if (overlaps.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
 }
